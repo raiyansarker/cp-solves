0b64724d55828c46785894dd760f0a5c3551da4b
Raiyan Sarker <developerraiyan@gmail.com>
2025-11-22 23:29:57 +0600

    weird algorithm cses implementation

---
+++ cses/introductory/weird-algorithm.cpp
@@ -0,0 +1,43 @@
+// ██████████████████╗█████╗███╗   ██╗    ██████╗ █████╗████╗   ██╗█████╗███╗   ██╗
+// ╚══██╔══██╚══██╔══██╔══██████╗  ██║    ██╔══████╔══████╚██╗ ██╔██╔══██████╗  ██║
+//    ██║  ██║  ██║  █████████╔██╗ ██║    ██████╔█████████║╚████╔╝█████████╔██╗ ██║
+//    ██║  ██║  ██║  ██╔══████║╚██╗██║    ██╔══████╔══████║ ╚██╔╝ ██╔══████║╚██╗██║
+//    ██║  ██║  ██║  ██║  ████║ ╚████║    ██║  ████║  ████║  ██║  ██║  ████║ ╚████║
+//    ╚═╝  ╚═╝  ╚═╝  ╚═╝  ╚═╚═╝  ╚═══╝    ╚═╝  ╚═╚═╝  ╚═╚═╝  ╚═╝  ╚═╝  ╚═╚═╝  ╚═══╝
+// GitHub: https://github.com/raiyansarker/cp
+// Question: https://cses.fi/problemset/task/1068/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;typedef unsigned long long ull;typedef long double ld;typedef pair<int,int> pii;typedef pair<ll,ll> pll;typedef vector<int> vi;typedef vector<ll> vll;typedef vector<pii> vpii;typedef vector<pll> vpll;
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+
+#ifndef NDEBUG
+namespace dbg{string s(int x){return to_string(x);}string s(long long x){return to_string(x);}string s(unsigned long long x){return to_string(x);}string s(double x){return to_string(x);}string s(long double x){return to_string(x);}string s(char x){return "'"+string(1,x)+"'";}string s(string x){return '"'+x+'"';}string s(bool x){return x?"true":"false";}string s(const char*x){return'"'+string(x)+'"';}template<class A,class B>string s(pair<A,B>p){return"("+s(p.first)+","+s(p.second)+")";}template<class T>string s(T v){string r="{";for(auto x:v)r+=(r=="{"?"":","),r+=s(x);return r+"}";}template<class T>string s(stack<T>st){string r="{";while(!st.empty())r+=(r=="{"?"":","),r+=s(st.top()),st.pop();return r+"}";}template<class T>string s(queue<T>q){string r="{";while(!q.empty())r+=(r=="{"?"":","),r+=s(q.front()),q.pop();return r+"}";}template<class T>string s(priority_queue<T>pq){string r="{";while(!pq.empty())r+=(r=="{"?"":","),r+=s(pq.top()),pq.pop();return r+"}";}void p(const char*n){cerr<<"\n";}template<class T>void p(const char*n,T v){cerr<<n<<"="<<s(v)<<"\n";}template<class T,class...A>void p(const char*n,T v,A...a){for(;*n!=',';n++)cerr<<*n;cerr<<"="<<s(v)<<", ";p(n+1,a...);}}
+#define debug(...) cerr<<"[L"<<__LINE__<<"]: ",dbg::p(#__VA_ARGS__,__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+    
+  ll n; cin >> n;
+  bool flag = false;
+  while (n != 1) {
+    cout << (flag ? " " : "") << n;
+    if (n % 2 == 0) n /= 2;
+    else n = 3*n + 1;
+    flag = true;
+  }
+  cout << " 1\n";
+    
+  return 0;
+}
